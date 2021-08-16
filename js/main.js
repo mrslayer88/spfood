65fafcd77d94b8f10b80f235eb39b370bbe8f992
mrslayer88 <45197153+mrslayer88@users.noreply.github.com>
2021-08-16 12:32:14 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// Убавляем кол-во по клику
+$('.quantity_inner .bt_minus').click(function() {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+});
+// Прибавляем кол-во по клику
+$('.quantity_inner .bt_plus').click(function() {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) + 1;
+    count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
+    $input.val(parseInt(count));
+}); 
+// Убираем все лишнее и невозможное при изменении поля
+$('.quantity_inner .quantity').bind("change keyup input click", function() {
+    if (this.value.match(/[^0-9]/g)) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    if (this.value == "") {
+        this.value = 1;
+    }
+    if (this.value > parseInt($(this).data('max-count'))) {
+        this.value = parseInt($(this).data('max-count'));
+    }    
+});
+$(function(){
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"
+        }, {
+            duration: 700,
+            easing: 'swing'
+            });
+            return false;
+    });
+});
+
+$('.callback-btn').on('click', function() {
+    $('.callback').addClass('active');
+});
+$('.close-modal-btn').on('click', function() {
+    $('.modal').removeClass('active');
+});
+$('.cart-btn').on('click', function() {
+    $('.cart').addClass('active');
+});
+$('.programm-btn').on('click', function() {
+    $('.cart-added').addClass('active');
+});
